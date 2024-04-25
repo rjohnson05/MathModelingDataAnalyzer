d0048ba19a5ba7f534841e09a9bab03745181260
Ryan Johnson <rjohnson@carroll.edu>
2024-04-24 20:28:31 -0600

    Added database population method and method to create an output file describing several pieces of information about the data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sqlite3
 import pandas as pd
 
 """
@@ -65,11 +66,13 @@ def validate_dataframe(df):
 
 def prepare_data(df):
     """
-    Loops through a dataframe with the necessary columns and creates two spreadsheets: one containing only data about
+    Loops through a dataframe with the necessary columns and creates two dataframes: one containing only data about
     institutions and the other only containing information about participating teams.
 
-    :param df: Pandas to be used when creating new spreadsheets
+    :param df: Pandas dataframe to be used when creating new spreadsheets
+    :return: Two Pandas dataframes: one containing only data about institutions and the other only data about teams
     """
+    print("Preparing data for the database...")
     # Rename the institutions column if it has junk in the column name
     for column_name in df.columns:
         if "Institution" in column_name:
@@ -84,7 +87,7 @@ def prepare_data(df):
     for index, row in df.iterrows():
         # Add institution data from the row, cleaning the data before entering it into the new spreadsheet
         institution = row["Institution"]
-        cleaned_institution = institution.split(",")[0].strip()
+        cleaned_institution = institution.split(", - ( )")[0].strip()
         lowercase_institution = cleaned_institution.lower()
         if lowercase_institution not in inst_ids.keys():
             inst_id_counter += 1
@@ -92,8 +95,10 @@ def prepare_data(df):
             row_institution = pd.DataFrame({'Institution ID': [inst_ids[lowercase_institution]],
                                             'Institution Name': [lowercase_institution.title()],
                                             'City': [row['City'].lower().title()],
-                                            'State/Province': [row['State/Province'].lower().title() if type(row['State/Province']) is str else None],
-                                            'Country': [row['Country'].lower().title() if type(row['Country']) is str else None]})
+                                            'State/Province': [row['State/Province'].lower().title() if type(
+                                                row['State/Province']) is str else None],
+                                            'Country': [row['Country'].lower().title() if type(
+                                                row['Country']) is str else None]})
             institutions_df = pd.concat([institutions_df, row_institution], ignore_index=True)
             institutions_df = institutions_df.reset_index(drop=True)
 
@@ -107,14 +112,148 @@ def prepare_data(df):
             teams_df = pd.concat([teams_df, row_team], ignore_index=True)
             teams_df = teams_df.reset_index(drop=True)
 
-    # Generate institution and team .csv files using these created dataframes
-    institutions_df.to_csv('Institutions.csv', index=False)
-    teams_df.to_csv('Teams.csv', index=False)
+    return institutions_df, teams_df
+
+
+def populate_db(inst_df, teams_df):
+    """
+    Populates a SQLite database with the two cleaned dataframes. One dataframe contains institution data, and the other
+    contains team data. Within the database, a table is created from each of the two dataframes. This database is later
+    queried to produce a .txt file containing a few statistics.
+
+    :param inst_df: Pandas dataframe containing data about the institutions participating in the competition
+    :param teams_df: Pandas dataframe containing data about the teams participating in the competition
+    """
+    print("Populating the database...")
+    conn = sqlite3.connect('math_competition.db')
+    cursor = conn.cursor()
+
+    # Populate the Institutions table
+    cursor.execute("CREATE TABLE IF NOT EXISTS institutions (id INTEGER PRIMARY KEY, name VARCHAR, city VARCHAR, "
+                   "state_province VARCHAR, country VARCHAR)")
+    for index, row in inst_df.iterrows():
+        name = row['Institution Name']
+        cursor.execute("SELECT id FROM institutions WHERE name=?", (name,))
+        # Ensure that no duplicate entries are placed into the DB
+        if cursor.fetchone() is not None:
+            print(f"Didn't put duplicate institution into the DB ({name})")
+            continue
+
+        inst_id = row['Institution ID']
+        city = row['City']
+        state_province = row['State/Province']
+        country = row['Country']
+        cursor.execute("INSERT INTO institutions (id, name, city, state_province, country) VALUES (?, ?, ?, ?, ?)",
+                       (inst_id, name, city, state_province, country))
+
+    # Populate the Teams table
+    cursor.execute("CREATE TABLE IF NOT EXISTS teams (id INTEGER PRIMARY KEY, advisor VARCHAR, problem VARCHAR, "
+                   "ranking VARCHAR, institution_id INTEGER, FOREIGN KEY (institution_id) REFERENCES institutions(id))")
+    for index, row in teams_df.iterrows():
+        team_id = row['Team Number']
+        cursor.execute("SELECT id FROM teams WHERE id=?", (team_id,))
+        # Ensure that no duplicate entries are placed into the DB
+        if cursor.fetchone() is not None:
+            print(f"Didn't put duplicate team into the DB (#{team_id})")
+            continue
+
+        advisor = row['Advisor']
+        problem = row['Problem']
+        ranking = row['Ranking']
+        inst_id = row['Institution ID']
+        cursor.execute("INSERT INTO teams (id, advisor, problem, ranking, institution_id) VALUES (?, ?, ?, ?, ?)",
+                       (team_id, advisor, problem, ranking, inst_id))
+
+    conn.commit()
+    conn.close()
+
+
+def make_queries():
+    """
+    Queries the database to find the following pieces of information from the data:
+      - Average number of teams entered per institution
+      - Institutions that entered the most teams, including the number of teams that they entered
+      - Institutions whose team(s) earned 'Outstanding' rankings (ordered by institution name)
+      - US teams who received 'Meritorious' ranking or better
+
+    :return: Results from each of the queries, each separate query result being contained in a list
+    """
+    conn = sqlite3.connect('math_competition.db')
+    cursor = conn.cursor()
+
+    # Determine average number of teams entered per institution
+    cursor.execute("SELECT COUNT(*) FROM teams")
+    num_teams = cursor.fetchall()[0][0]
+    cursor.execute("SELECT COUNT(*) FROM institutions")
+    num_insts = cursor.fetchall()[0][0]
+    mean_num_teams = num_teams / num_insts
+
+    # Determine the institutions that entered the most teams, including the number of teams that they entered
+    cursor.execute("SELECT i.name, COUNT(*) "
+                   "AS num_teams "
+                   "FROM teams t "
+                   "INNER JOIN institutions i ON i.id = t.institution_id "
+                   "GROUP BY institution_id "
+                   "ORDER BY num_teams DESC")
+    ordered_insts = cursor.fetchall()
+
+    # Determine the institutions whose team(s) earned 'Outstanding' rankings (ordered by institution name)
+    cursor.execute("SELECT name "
+                   "FROM institutions "
+                   "WHERE id IN (SELECT institution_id FROM teams WHERE ranking='Outstanding Winner') "
+                   "ORDER BY name")
+    outstanding_insts = cursor.fetchall()
+
+    # Determine the US teams who received 'Meritorious' ranking or better
+    cursor.execute("SELECT t.id "
+                   "FROM teams t "
+                   "INNER JOIN institutions i ON t.institution_id=i.id "
+                   "WHERE t.ranking IN ('Outstanding Winner', 'Finalist', 'Meritorious') AND i.country='Usa'")
+    usa_meritorious_teams = cursor.fetchall()
+
+    return mean_num_teams, ordered_insts, outstanding_insts, usa_meritorious_teams
+
+
+def create_output_file(result1, result2, result3, result4):
+    """
+    Creates a .txt file that displays the following information from the database:
+      - Average number of teams entered per institution
+      - Institutions that entered the most teams, including the number of teams that they entered
+      - Institutions whose team(s) earned 'Outstanding' rankings (ordered by institution name)
+      - US teams who received 'Meritorious' ranking or better
+
+    :param result1: List containing average number of teams entered per institution
+    :param result2: List of tuples, the first element being the name of an institution and the second
+                    element being the number of teams entered from the institution
+    :param result3: List of all institutions that had at least one team with a ranking of 'Outstanding Winner'
+    :param result4: List of ID numbers for all US teams that received ranking of 'Meritorious' or better
+    """
+    with open("results.txt", "w") as f:
+        f.write(f"Average Number of Teams per Institution: {round(result1, 2)} teams\n")
+
+        f.write("\nInstitutions Ordered by Number of Teams:\n")
+        for inst in result2:
+            f.write(f"  - {inst[0]}: {inst[1]} team(s)\n")
+
+        f.write("\nInstitutions with Outstanding Winner(s):\n")
+        for inst in result3:
+            f.write(f"  - {str(inst[0])}\n")
+
+        f.write("\nUS Teams Receiving Ranking of Meritorious or Better:\n")
+        for inst in result4:
+            f.write(f"  - {str(inst[0])}\n")
 
 
 if __name__ == "__main__":
-    print("Welcome to the Math Modeling Contest spreadsheet preparer. To prepare a spreadsheet for a SQL database, "
+    print("Welcome to the Math Modeling Contest spreadsheet analyzer. To analyze data from a year's contest, "
           "enter the name of a valid .csv file.")
     df = get_input_dataframe()
-    prepare_data(df)
-    print("Institution & Team spreadsheets created")
+    if not validate_dataframe(df):
+        exit(1)
+
+    inst_df, teams_df = prepare_data(df)
+    populate_db(inst_df, teams_df)
+    result1, result2, result3, result4 = make_queries()
+    create_output_file(result1, result2, result3, result4)
+
+    print("Your results file has been created and titled 'results.txt'")
